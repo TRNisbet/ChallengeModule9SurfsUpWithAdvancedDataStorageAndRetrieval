1a7f85a6de800437348e0c09353e4a26465c8d76
TRNisbet <TRNisbet@hotmail.com>
2023-01-21 14:05:50 -0500

    all work done except for the analysis

---
+++ app.py
@@ -0,0 +1,15 @@
+# Import Flask
+from flask import Flask
+
+# # create a new Flask instance called app
+app = Flask(__name__)
+
+# # Create Flask Routes
+@app.route('/')
+def hello_world():
+    return 'Testing to Make Sure Flask is Working'
+
+
+# set FLASK_APP=app.py <-Ran in terminal
+
+# flask run <-Ran in terminal
---
+++ surfs_up/README.md
@@ -0,0 +1,19 @@
+# Challenge Module- 9
+## Surfs Up with Advanced Data Storage And Retrieval
+
+## Module 9 Challenge
+
+### Overview
+
+
+### Results
+
+
+![img](images/june.jpg)
+
+
+![img](images/december.jpg)
+
+
+### Summary
+
